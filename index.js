e1aff7a9e7cb792cc53129cfd47fe8f864daab1b
J.Reinhardt <grow435@gmail.com>
2024-07-02 23:11:51 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.getElementById("count-el").innerText = 5
+let count = 0;
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let savedEntries = count + " - "
+    saveEl.textContent += savedEntries
+    countEl.textContent = 0
+    count = 0
+
+}
+
